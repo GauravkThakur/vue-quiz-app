102c83a4a689a58d81804df0586160944b805bfc
Milad Dehghan <milad.dehghan.sheyjani@nl.abnamro.com>
2024-11-01 16:58:08 +0100

    feat: add beatify to code blocks

--- src/components/QuizSyntaxHighlighter.vue
+++ src/components/QuizSyntaxHighlighter.vue
@@ -1,7 +1,7 @@
 <template>
-  <div ref="quizSyntaxHighlighter" v-bind="$attrs">
+  <div v-bind="$attrs">
     <pre>
-      <code>
+      <code ref="quizSyntaxHighlighter">
         <slot />
       </code>
     </pre>
@@ -13,15 +13,45 @@ import hljs from 'highlight.js/lib/core';
 import xml from 'highlight.js/lib/languages/xml';
 import javascript from 'highlight.js/lib/languages/javascript';
 import { onMounted, ref } from 'vue';
+import { html as beautifyHtml, js as beautifyJs, css as beautifyCss } from 'js-beautify';
+
 import 'highlight.js/styles/default.css'; // Import the Highlight.js CSS theme
 
 const quizSyntaxHighlighter = ref<HTMLElement | null>(null);
 hljs.registerLanguage('xml', xml);
 hljs.registerLanguage('javascript', javascript);
 
-onMounted(() => {
+const props = defineProps({
+  language: {
+    type: String,
+    required: true
+  }
+});
+
+function escapeHtml(html: string): string {
+  return html.replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
+
+
+onMounted(async () => {
   if (quizSyntaxHighlighter.value) {
-    hljs.highlightElement(quizSyntaxHighlighter.value);
+    const code = quizSyntaxHighlighter.value.innerText;
+    let formattedCode = code;
+
+    if (props.language === 'html') {
+      formattedCode = beautifyHtml(code);
+    } else if (props.language === 'javascript') {
+      formattedCode = beautifyJs(code);
+    } else if (props.language === 'css') {
+      formattedCode = beautifyCss(code);
+    }
+
+    quizSyntaxHighlighter.value.innerHTML = escapeHtml(formattedCode);
+    hljs.highlightElement(quizSyntaxHighlighter.value as HTMLElement);
   }
 });
 </script>
